cec531a84369c1f9ca5a2ca2ab4bc5d16360acee
Matthijs Spaan <matthijs.spaan@centric.eu>
2020-02-04 14:04:56 +0200

    Updates

---
+++ precache-manifest.00a593b7ff2f28fcbae04fa1bd1ab5e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d6676ca514b1565f4cb1e6fe339bc5b",
+    "url": "/Snake/index.html"
+  },
+  {
+    "revision": "a0a06d09e89e20146777",
+    "url": "/Snake/static/css/main.a315a83b.chunk.css"
+  },
+  {
+    "revision": "43858a66ec59939fcb3e",
+    "url": "/Snake/static/js/2.74087b4a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Snake/static/js/2.74087b4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0a06d09e89e20146777",
+    "url": "/Snake/static/js/main.f950a6e9.chunk.js"
+  },
+  {
+    "revision": "95e8180567f54fa89cb5",
+    "url": "/Snake/static/js/runtime-main.a5adae72.js"
+  }
+]);
